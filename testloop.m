80f368c51749516dbba33bbfe5287db54bbbb713
cbeltran <carlos.beltran@iit.it>
2013-04-09 16:58:42 +0200

    Now showing the image

--- testloop.m
+++ testloop.m
@@ -1,5 +1,11 @@
 while true
-    res = newimagesindir('Y:\# DATASETS\IITIntervalsSeq28_03_2013\Seq000\');
-    res(:).name
+    path = 'Y:\# DATASETS\IITIntervalsSeq28_03_2013\Seq000\';
+    res = newimagesindir(path);
+    %res(:).name
+    for j = 1: length(res)
+        res(j).name
+        imgTmp = imread([path res(j).name]);
+        imshow(imgTmp);
+    end
     pause(1)
 end
