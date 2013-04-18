68b2709ff8aba0dee121c6af9a7879ce751deab4
cbeltran <carlos.beltran@iit.it>
2013-04-18 15:06:53 +0200

    Visualizing the last snapshot

--- testloop.m
+++ testloop.m
@@ -1,11 +1,19 @@
 while true
-    path = 'Y:\# DATASETS\IITIntervalsSeq28_03_2013\Seq000\';
+    %path = 'Y:\# DATASETS\IITIntervalsSeq28_03_2013\Seq000\';
+    path = 'Y:\DemoReID\CAM_0\';
     res = newimagesindir(path);
-    %res(:).name
-    for j = 1: length(res)
-        res(j).name
-        imgTmp = imread([path res(j).name]);
-        imshow(imgTmp);
+    res(:).name
+    % Visualize last one
+    last = length(res);
+    if last ~= 0
+        imgTmp = imread([path res(last).name]);
+        imshow(imgTmp)
     end
+    fprintf('Got %i new images.\n',last)
+    %for j = 1: length(res)
+    %    res(j).name
+    %    imgTmp = imread([path res(j).name]);
+    %    imshow(imgTmp);
+    %end
     pause(1)
 end
